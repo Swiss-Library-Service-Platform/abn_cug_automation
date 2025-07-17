9253988f8e1c3db0c3da2be96e0cc3361321872a
Raphaël Rey <raphael.rey@slsp.ch>
2025-07-17 14:27:22 +0200

    update to 2025 version

--- config.py
+++ config.py
@@ -9,14 +9,14 @@ HFGS_USER_GROUP_CODE = 'ABN_HFGS_Patron-HFGS'
 VERWALTUNG_USER_GROUP_CODE = 'ABN_Patron-Kantonale-Verwaltung'
 
 # Local files path
-REPOSITORY_PATH = './abn_slsp_exchange'
+REPOSITORY_PATH = './abn_slsp_exchange_2025'
 PATH_TO_DATA_CURRENT_STATE = f'{REPOSITORY_PATH}/CUG_MEDIO_current_state.csv'
 PATH_TO_SOURCE_DATA = f'{REPOSITORY_PATH}/test_list_encrypted.bin'
 PATH_TO_REPORT_MEDIOTHEKEN = f'{REPOSITORY_PATH}/report_cug_mediotheken.csv'
 PATH_TO_REPORT_VERWALTUNG = f'{REPOSITORY_PATH}/report_cug_verwaltung.csv'
 
 # Git repository configuration
-REPOSITORY_URL = 'git.ag.ch/abn/abn_slsp_exchange.git'
+REPOSITORY_URL = 'git.ag.ch/abn/abn_slsp_exchange_2025.git'
 REPOSITORY_TOKEN_KEY = 'rw_slsp_token'
 
 # Analytics report configuration
